c62964ecaaea3fc0c5833140c5a996a56f6684c9
Chung-Yu Wu <ms0448711ms0448711@gmail.com>
2022-06-03 21:44:16 +0800

    Add an example app

---
+++ WinFormsApp/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using OCRLibrary;
+using Gma.System.MouseKeyHook;
+
+namespace WinFormsApp
+{
+    public partial class Form1 : Form
+    {
+
+        private ImageCapturer ic = new ImageCapturer();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            m_GlobalHook = Hook.GlobalEvents();
+            m_GlobalHook.KeyPress += GlobalHookKeyPress;
+        }
+        private IKeyboardMouseEvents m_GlobalHook;
+
+        private void GlobalHookKeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 's')
+            {
+                ic.storeCursorPos.Invoke();
+                label1.Text = ic.GetCursorsString;
+            }
+            if (e.KeyChar == 'd')
+            {
+                pictureBox1.Image = Image.FromHbitmap(ic.GetImageFromCursors().GetHbitmap());
+                label2.Text=ic.GetTexts();
+            }
+        }
+
+
+    }
+}
